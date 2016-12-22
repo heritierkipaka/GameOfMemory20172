c8d3079c90d8391d7aea76e2a676637b5c2104c7
Kipaka <Mubwala.Kipaka@saint-gobain.com>
2016-12-22 11:30:54 +0100

    Initial commit

---
+++ app/src/main/java/fr/kipaka/com/gameofmemory2017/MainActivity.java
@@ -0,0 +1,13 @@
+package fr.kipaka.com.gameofmemory2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
